189f2c78047948fc2b5a6bc4d65de81915d58b25
MiaguawU <isis.miaguaw@gamil.com>
2024-12-14 13:57:02 -0600

    porfin?

--- server/base/connection.js
+++ server/base/connection.js
@@ -1,21 +1,26 @@
-const mysql = require('mysql2');
+const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
+  ssl: {
+    rejectUnauthorized: false, // Requerido para conexiones SSL con Render
+  },
 });
 
-db.connect((err) => {
+pool.connect((err, client, release) => {
   if (err) {
     console.error('Error al conectar con la base de datos:', err.message);
     process.exit(1);
   }
   console.log('Se pudo conectar a la base');
+  release(); // Libera el cliente después de la conexión
 });
 
-module.exports = db;
+module.exports = pool;
